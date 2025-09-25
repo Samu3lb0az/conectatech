9e1247c31ecfc405bb449e51400ee960323670c2
Samu3lb0az <samuelboaz6@gmail.com>
2025-09-25 10:07:05 -0300

    db

--- sql/redesocial_db.sql
+++ sql/redesocial_db.sql
@@ -4,74 +4,106 @@ USE `redesocial_db`;
 START TRANSACTION;
 
 CREATE TABLE `users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-  `full_name` VARCHAR(255) NOT NULL,
-  `bio` TEXT DEFAULT NULL,
-  `profile_picture` BLOB NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `bio` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `users` (`id`, `email`, `password`, `full_name`, `bio`, `created_at`) VALUES
+(1, 'samuel@exemple.com', '$2y$10$e.XRWygEaEX7us3vi6h/POxsPPY0i0GOmColmmKF19aXsNBGwPHGO', 'Samuel', NULL, '2025-09-18 11:48:26'),
+(2, 'andrey@gmail.com', '$2y$10$eoPJ4j7hfNx6/evQFisPwOLuVivhjNqM.WD0hY/rJf57cVT/3/mKq', 'Andrey', NULL, '2025-09-18 12:01:06');
+
 CREATE TABLE `posts` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `user_id` INT(11) NOT NULL,
-  `conteudo` TEXT DEFAULT NULL,
-  `imagem` LONGBLOB DEFAULT NULL,
-  `descricao` VARCHAR(255) DEFAULT NULL,
-  `created_at` DATETIME DEFAULT current_timestamp(),
-  `mime_type` VARCHAR(50) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `conteudo` text DEFAULT NULL,
+  `imagem` longblob DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `mime_type` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `posts` (`id`, `user_id`, `conteudo`, `imagem`, `descricao`, `created_at`, `mime_type`) VALUES
+(1, 1, 'nada', NULL, NULL, '2025-09-18 08:49:12', NULL),
+(2, 1, 'como', NULL, NULL, '2025-09-18 08:52:51', NULL),
+(3, 1, 'OLAHAAAAAAAAAA', NULL, NULL, '2025-09-18 11:03:19', NULL);
+
 CREATE TABLE `comments` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `user_id` INT(11) NOT NULL,
-  `post_id` INT(11) NOT NULL,
-  `comment` TEXT NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `fk_comments_posts` (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `comments` (`id`, `user_id`, `post_id`, `comment`, `created_at`) VALUES
+(1, 1, 1, 'olha', '2025-09-18 11:49:36'),
+(2, 1, 2, 'olaaaaaaaa', '2025-09-18 11:53:01');
+
 CREATE TABLE `followers` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `follower_id` INT(11) NOT NULL,
-  `following_id` INT(11) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `follower_id` int(11) NOT NULL,
+  `following_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_follow` (`follower_id`, `following_id`),
+  UNIQUE KEY `unique_follow` (`follower_id`,`following_id`),
   KEY `following_id` (`following_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `likes` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `user_id` INT(11) NOT NULL,
-  `post_id` INT(11) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_like` (`user_id`, `post_id`),
-  KEY `likes_ibfk_2` (`post_id`)
+  UNIQUE KEY `unique_like` (`user_id`,`post_id`),
+  KEY `post_id` (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `likes` (`id`, `user_id`, `post_id`, `created_at`) VALUES
+(3, 2, 3, '2025-09-18 14:03:32');
+
 CREATE TABLE `messages` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `sender_id` INT(11) NOT NULL,
-  `receiver_id` INT(11) NOT NULL,
-  `message` TEXT NOT NULL,
-  `is_read` TINYINT(1) DEFAULT 0,
-  `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `is_read` tinyint(1) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `sender_id` (`sender_id`),
   KEY `receiver_id` (`receiver_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `messages` (`id`, `sender_id`, `receiver_id`, `message`, `is_read`, `created_at`) VALUES
+(1, 2, 1, 'ola, td bem', 1, '2025-09-18 12:04:24'),
+(2, 1, 2, 'olaaaaaaaa, bundão', 1, '2025-09-18 12:05:06'),
+(3, 2, 1, 'fala tiozão', 1, '2025-09-18 12:06:54');
+
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
@@ -88,5 +120,8 @@ ALTER TABLE `likes`
 ALTER TABLE `messages`
   ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 COMMIT;
